304696db5ed1e595d9c63ab268a42fb7ce02253a
MSlater <megan.e.slater@gmail.com>
2017-05-25 13:06:08 -0700

    cleanup for inspection

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -2,39 +2,35 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   firstName: null,
-
   lastName: null,
-
   actions: {
-    createUser() {
-      // const user = this.get('store').createRecord('user', {
-      //   firstName: this.get('firstName'),
-      //   lastName: this.get('lastName')
-      // });
-      // return user.save();
-      Ember.$.ajax('https://simple-investing-app.herokuapp.com/api/users', {
-        method: 'POST',
-        dataType: 'json',
-        contentType: 'application/json',
-        crossDomain: true,
-        data: JSON.stringify({
-          data: {
-            attributes: {
-              'first-name': 'asdfasd',
-              'last-name': 'asdfasdf'
-            }
-          }
-        })
-      })
+    createuser() {
+      const user = this.get('store').createRecord('user', {
+        firstName: this.get('firstName'),
+        lastName: this.get('lastName')
+      });
+      return user.save();
+      // Ember.$.ajax('https://simple-investing-app.herokuapp.com/api/users', {
+      //           method: 'POST',
+      //           dataType: 'json',
+      //           contentType: 'application/json',
+      //           crossDomain: true,
+      //           data: JSON.stringify({
+      //             data: {
+      //               attributes: {
+      //                   'first-name': 'asdfasd',
+      //                     'last-name': 'asdfasdf'
+      //                 }
+      //             }
+      //         })
+      //       })
     },
-
     updateUser(user) {
       return user.save();
     },
-
     deleteUser(user) {
       user.deleteRecord();
-      user.save()
+      return user.save();
     }
   }
 });
